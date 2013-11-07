5677d35ee1426197a7d46aacd7268fa3c49af844
Huub Bouma <bouma@w20e.com>
2013-11-07 11:09:34 +0100

    abbreviate_hellip filter added

--- djinn_contenttypes/templatetags/djinn_contenttypes.py
+++ djinn_contenttypes/templatetags/djinn_contenttypes.py
@@ -1,3 +1,4 @@
+# coding=utf-8
 from django.template import Library
 
 
@@ -5,12 +6,22 @@ register = Library()
 
 
 @register.filter
-def abbreviate(text, length=50):
+def abbreviate(text, length=50, truncate_str='...'):
     """
     Usage: {{ sometext|abbreviate:"50" }} Abbreviates given string if
     it is more than 'length' chars, and adds trailing dots.
     """
 
     if len(text) > length:
-        return "%s..." % text[:length]
+        return "%s%s" % (text[:length], truncate_str)
     return text
+
+@register.filter
+def abbreviate_hellip(text, length=50):
+    """
+    Usage: {{ sometext|abbreviate:"50" }} Abbreviates given string if
+    it is more than 'length' chars, and adds trailing horizontal ellipsis.
+    """
+
+    return abbreviate(text, length, u'…')
+
