d852d93acefa556fb30b9d9ee1d5f42a4c61843f
Duco Dokter <dokter@w20e.com>
2014-03-04 22:45:39 +0100

    Hotfix release

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ requires = [
     ]
 
 setup(name='djinn_contenttypes',
-      version="1.1.0",
+      version="1.1.1",
       description='Djinn Intranet Contenttypes framework',
       long_description=README + '\n\n' +  CHANGES,
       classifiers=[
