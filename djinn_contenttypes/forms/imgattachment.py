89576cdda0af0406b0f36f8bbc25d98ed513da50
Duco Dokter <dokter@w20e.com>
2014-05-27 13:29:38 +0200

    Prepeare release 1.2.3

--- djinn_contenttypes/forms/imgattachment.py
+++ djinn_contenttypes/forms/imgattachment.py
@@ -5,14 +5,6 @@ from djinn_contenttypes.forms.base import MetaFieldsMixin
 
 class ImgAttachmentForm(MetaFieldsMixin, forms.ModelForm):
 
-    def clean_title(self):
-
-        _title = self.cleaned_data.get('title')
-
-        import pdb; pdb.set_trace()
-
-        pass
-
     class Meta:
         model = ImgAttachment
         fields = ["title"]
