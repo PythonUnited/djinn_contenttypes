846d92001413bd85fbb93a27a3eb79208c25ad7e
Duco Dokter <dokter@w20e.com>
2015-02-11 16:58:19 +0100

    hide highlight for non-authorized users

--- djinn_contenttypes/models/highlight.py
+++ djinn_contenttypes/models/highlight.py
@@ -4,6 +4,7 @@ from django.contrib.contenttypes.models import ContentType
 from django.contrib.contenttypes import generic
 from django.utils.translation import ugettext_lazy as _
 
+
 class Highlight(models.Model):
 
     """Hightlight given content. This can be used to set an additional
