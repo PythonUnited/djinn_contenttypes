34619a105f9bb3d5763a1b76571f89d1c3870047
Duco Dokter <dokter@w20e.com>
2015-06-18 11:55:35 +0200

    prepare 1.4.4

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ requires = [
     ]
 
 setup(name='djinn_contenttypes',
-      version="1.4.3",
+      version="1.4.4",
       description='Djinn Intranet Contenttypes framework',
       long_description=README + '\n\n' + CHANGES,
       classifiers=[
