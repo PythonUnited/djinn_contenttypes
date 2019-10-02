b5c32fbe27f719b1f10a6889e63565496aa61af8
Martin Bouma <martin@studioreactor.com>
2019-10-02 14:40:59 +0200

    better ratio

--- djinn_contenttypes/settings.py
+++ djinn_contenttypes/settings.py
@@ -7,7 +7,7 @@ DEFAULT_DATE_INPUT_FORMAT = getattr(
     settings, 'DEFAULT_DATE_INPUT_FORMAT', '%d/%m/%Y')
 
 FEED_HEADER_HIGH_SIZE = getattr(
-    settings, 'FEED_HEADER_HIGH_SIZE', [800, 600])
+    settings, 'FEED_HEADER_HIGH_SIZE', [1000, 500])
 
 FEED_HEADER_NORMAL_SIZE = getattr(
     settings, 'FEED_HEADER_NORMAL_SIZE', [800, 600])
