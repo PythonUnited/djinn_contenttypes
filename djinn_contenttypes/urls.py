6340e66a067654155ccb647858ecb823cd779d3c
Martin Bouma <martin@studioreactor.com>
2022-05-06 14:04:50 +0200

    voorbereiding op v3

--- djinn_contenttypes/urls.py
+++ djinn_contenttypes/urls.py
@@ -14,6 +14,12 @@ urlpatterns = [
         ShareActivityView.as_view(),
         name="djinn_contenttypes_share"),
 
+    url(r'^gronet_v3/content/share/(?P<activity_id>[\d]+)/?$',
+        ShareActivityView.as_view(
+            template_name="gronet_v3/djinn_contenttypes/snippets/share.html"
+        ),
+        name="djinn_contenttypes_share_v3"),
+
     url(r'^content/preview/(?P<ctype>[\w]+)/(?P<id>[\d]+)/?$',
         FeedPreview.as_view(),
         name="djinn_contenttypes_preview"),
